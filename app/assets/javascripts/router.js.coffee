f1665228e135b6c881e7a7c7ddeaefec77f36467
Kathryn <keb97@cornell.edu>
2013-08-30 23:40:14 -0700

    app on Rails 3.2.13

---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+TravelSpire.Router.map ()->
+  # @resource('posts')
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelSpire::Application.config.secret_token = '43557ff030d93c9103c9a9fd5f89adcf2f5359bf655c7c33b856481cd09a6c8fbc53596eea50526fcc3d435a2aff464a4fedc80cd16ba97df5b52827303d7f4a'
